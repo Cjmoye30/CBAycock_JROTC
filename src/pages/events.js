237ae24e18793a9516c954fe161ff53ee1939e01
CJMoye <Cjmoye30@gmail.com>
2023-09-08 18:49:50 -0400

    Pulling all events and filtering and sorting based on dates

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { gapi } from "gapi-script";
-import Event from "../components/event";
+import moment from "moment/moment";
 
 function Events() {
     const [events, setEvents] = useState([]);
@@ -23,8 +23,25 @@ function Events() {
                 .then(
                     (response) => {
                         let events = response.result.items;
-                        console.log(events)
-                        setEvents(events);
+                        console.log("All events data: ",events)
+
+                        let sortedEvents = events.sort((a,b) => {
+                            const dateA = new Date(a.start.date || a.start.dateTime)
+                            const dateB = new Date(b.start.date || b.start.dateTime)
+                            return dateA - dateB
+                        });
+
+                        let currentEvents = sortedEvents.filter((item) => {
+                            const eventDate = new Date(item.start.date || item.start.dateTime)
+                            const currentDate = new Date();
+                            return eventDate >= currentDate;
+                        })
+
+                        console.log("Only upcoming events: ", currentEvents);
+                        console.log("Closest Upcoming Event: ", currentEvents[0].summary)
+                        console.log("Dates sorted: ",sortedEvents.map(x => x.start));
+
+                        setEvents(currentEvents);
                     },
                     function (err) {
                         return [false, err];
@@ -36,28 +53,28 @@ function Events() {
 
     const addEvent = (calendarID, event) => {
         function initiate() {
-          gapi.client
-            .request({
-              path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
-              method: "POST",
-              body: event,
-              headers: {
-                "Content-type": "application/json",
-                Authorization: `Bearer ${accessToken}`,
-              },
-            })
-            .then(
-              (response) => {
-                return [true, response];
-              },
-              function (err) {
-                console.log(err);
-                return [false, err];
-              }
-            );
+            gapi.client
+                .request({
+                    path: `https://www.googleapis.com/calendar/v3/calendars/${calendarID}/events`,
+                    method: "POST",
+                    body: event,
+                    headers: {
+                        "Content-type": "application/json",
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                })
+                .then(
+                    (response) => {
+                        return [true, response];
+                    },
+                    function (err) {
+                        console.log(err);
+                        return [false, err];
+                    }
+                );
         }
         gapi.load("client", initiate);
-      };
+    };
 
     useEffect(() => {
         const events = getEvents(calendarID, apiKey);
@@ -66,10 +83,19 @@ function Events() {
 
     return (
         <>
+        <p>Todays date: {moment().format("MMMM Do, YYYY")} </p>
             <ul>
                 {events?.map((event) => (
                     <li key={event.id} className="flex justify-center">
-                        <Event description={event.summary} />
+                        <div>
+                            <h1>{event.summary}</h1>
+                            <h3>{event.description}</h3>
+                            <p>Date: {event.start.date || event.start.dateTime}</p>
+                            {event.attachments?.map((image, index) => (
+                                <p key={index}> <img className="eventImg" src={`https://drive.google.com/uc?id=${image.fileId}`} /> </p>
+                            ))}
+                        </div>
+
                     </li>
                 ))}
             </ul>
